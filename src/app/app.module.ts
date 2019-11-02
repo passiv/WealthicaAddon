410b7bc55bbbf10f2ad6eaff84332d451c6a7658
jacobcm <jacob-cm@outlook.com>
2019-11-02 16:21:07 -0400

    style change to mat buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatProgressSpinnerModule } from '@angular/material';
@@ -27,6 +28,7 @@ import { PortfolioOverviewComponent, EditPortfolioComponent, TradesNeededCompone
     BrowserAnimationsModule,
     FormsModule,
     MatButtonModule,
+    MatButtonToggleModule,
     MatCheckboxModule,
     MatRadioModule,
     MatSliderModule,
