3aca2e02090722944d2726edccb0bbc176e5f8bb
jacobcm <jacob-cm@outlook.com>
2022-02-17 17:24:11 -0500

    added auth token to services

--- src/app/services/passiv.service.ts
+++ src/app/services/passiv.service.ts
@@ -1,23 +1,32 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { PassivSymbolRequest, PassivTradeRequest } from '../models';
+
+const header = {
+  headers: new HttpHeaders()
+    .set('Authorization',  `Token c50416e736642a0e69a74512a9d14c4c633fd1cc`)
+}
+const options = {
+  headers: header,
+};
 
 @Injectable({
   providedIn: 'root'
 })
-export class PassivService {
 
+export class PassivService {
+  
   constructor(private http: HttpClient) { }
 
   search(request: PassivSymbolRequest) {
-    return this.http.post('https://api.passiv.com/api/v1/symbols', request);
+    return this.http.post('https://api.passiv.com/api/v1/symbols', request, header);
   }
 
   getTrades(request: PassivTradeRequest) {
-    return this.http.post('https://api.passiv.com/api/v1/embeddedTrades', request);
+    return this.http.post('https://api.passiv.com/api/v1/embeddedTrades', request, header);
   }
 
   getCurrencies() {
-    return this.http.get('https://api.passiv.com/api/v1/currencies/rates');
+    return this.http.get('https://api.passiv.com/api/v1/currencies/rates', header);
   }
 }
